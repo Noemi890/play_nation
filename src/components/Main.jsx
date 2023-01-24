136853a62ae74acff275626d186b9a7fd2196b4f
Noemi <noemi.caggiano@gmail.com>
2023-01-24 10:31:18 +0000

    chore: setup main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import gamesData from "../data/games.json"
+import {Card} from "./Card";
 
 const Main = () => {
+
+  const games = gamesData.games
+
+  console.log(games)
   return (
-    <>
-    </>
+    <main className="m-5">
+      <div className="grid" style={{ gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "10px" }}>
+        <Card src={games[0].background_image}/>
+        {/* {
+          games.map((game, i) => {
+            return (
+              <Card key={i} src={game}/>
+            )
+          })
+        } */}
+      </div>
+    </main>
   )
 }
 
