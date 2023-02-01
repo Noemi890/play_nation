d70404cccf5dbe7f752065b49d3bb907eb482a1a
Noemi <noemi.caggiano@gmail.com>
2023-02-01 14:55:00 +0000

    feat: make link for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-zinc-900 max-w-full h-14 flex justify-end items-center drop-shadow-lg sticky z-50 bottom-0">
-      <h3 className="text-slate-300 m-4">@Noemi Caggiano</h3>
+      <a href="https://github.com/Noemi890" rel="noreferrer" target="_blank" className="text-slate-300 m-4">@Noemi Caggiano</a>
     </div>
   );
 };
