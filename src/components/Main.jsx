d561d714ade21ebeea5bd6ae258c5b7ed26d859b
Noemi <noemi.caggiano@gmail.com>
2023-02-01 10:58:54 +0000

    feat: display all games

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,15 +9,14 @@ const Main = () => {
   console.log(games)
   return (
     <main className="m-5">
-      <div className="grid" style={{ gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "10px" }}>
-        <Card src={games[0].background_image}/>
-        {/* {
+      <div className="grid" style={{ gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "5px", rowGap: "20px" }}>
+        {
           games.map((game, i) => {
             return (
-              <Card key={i} src={game}/>
+              <Card key={i} game={game}/>
             )
           })
-        } */}
+        }
       </div>
     </main>
   )
