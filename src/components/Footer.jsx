ed89fe87ff4b9d65b54cc05e6c2954927550c0d0
Noemi <noemi.caggiano@gmail.com>
2023-02-01 14:50:25 +0000

    feat: add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div className="bg-zinc-900 max-w-full h-14 flex justify-end items-center drop-shadow-lg sticky z-50 bottom-0">
+      <h3 className="text-slate-300 m-4">@Noemi Caggiano</h3>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
