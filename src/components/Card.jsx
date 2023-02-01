47f9bdac80dbe26385ede13b3abbf4e38509db85
Noemi <noemi.caggiano@gmail.com>
2023-02-01 10:58:40 +0000

    feat: card component with css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import "../css/card.css"
+// import useNavigate from "react";
+import "../css/card.css";
 
-export const Card = ({ src }) => {
+export const Card = ({ game }) => {
+  // const navigate = useNavigate()
+
+  const handleClick = () => {
+    // navigate("/game", game)
+  }
+
   return (
-    <></>
-  )
-}
+    <button className="card_container" onClick={handleClick}>
+      <div>
+        <img
+          className="card_image"
+          src={game.background_image}
+          alt="cover_image"
+        />
+      </div>
+      <h2 className="card_title">{game.name}</h2>
+    </button>
+  );
+};
