5e445502e372966721362e50c6ff5caf6a21e899
Noemi <noemi.caggiano@gmail.com>
2023-02-01 15:04:55 +0000

    chore: setup react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Game from './pages/Game'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes>
+        <Route path='/' element={<Main />}/>
+        <Route path='/game' element={<Game />}/>
+      </Routes>
       <Footer />
     </div>
   );
