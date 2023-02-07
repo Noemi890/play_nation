d018771cfce4475b1478a245a20a19a6d681c8b7
Noemi <noemi.caggiano@gmail.com>
2023-02-07 15:55:25 +0000

    feat: footer animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-zinc-900 max-w-full h-14 flex justify-end items-center drop-shadow-lg sticky z-50 bottom-0">
-      <a href="https://github.com/Noemi890" rel="noreferrer" target="_blank" className="text-slate-300 m-4">@Noemi Caggiano</a>
+      <a
+        href="https://github.com/Noemi890"
+        rel="noreferrer"
+        target="_blank"
+        className="text-slate-300 m-4 hover:scale-125 transition duration-500 ease-in-out hover:text-sky-800"
+      >
+        @Noemi Caggiano
+      </a>
     </div>
   );
 };
