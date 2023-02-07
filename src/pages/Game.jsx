d6470732b9274abce969f2522dd5add6fcba3c3f
Noemi <noemi.caggiano@gmail.com>
2023-02-07 16:09:42 +0000

    feat: game view

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -9,12 +9,12 @@ const Game = () => {
   return (
     <>
       <div
-        className="flex grow flex-col bg-cover bg-top blur-sm relative"
+        className="flex grow flex-col bg-cover bg-top blur-sm relative overflow-hidden"
         style={{ backgroundImage: `url(${game.background_image})` }}
       ></div>
 
       <div
-        className="fixed top-28 left-32 bg-black/70 flex p-3 rounded-lg flex-col"
+        className="fixed top-28 left-32 bg-black/70 flex p-3 rounded-lg flex-col h-auto"
         style={{ width: "80vw", height: "75vh" }}
       >
         <div className="flex justify-between">
@@ -25,27 +25,27 @@ const Game = () => {
             alt={game.name}
           />
           </div>
-          <span className="text-slate-300 text-5xl">{game.name}</span>
-          <span className="text-slate-300 text-2xl">{game.release_date}</span>
+          <span className="text-slate-300 text-5xl self-center">{game.name}</span>
+          <span className="text-slate-300 text-2xl self-center">{game.release_date}</span>
         </div>
-        <div className="flex flex-row justify-between mt-6">
-          <div className="text-md w-1/2 text-slate-300">
+        <div className="flex flex-row justify-around mt-6">
+          <div className="text-md w-7/12 text-slate-300">
           <p>{game.description}</p>
           </div>
-          <ul>
-            <li>Genre: {
+          <ul className="text-slate-300 flex flex-col">
+            <li className="text-slate-300 flex">Genre:&nbsp;{
               game.genre.map((g, i) => {
-                return <li key={i}>{g}</li>
+                return <li key={i}>{`"${g}"`}&nbsp;</li>
               })
               }</li>
-            <li>Platform: {
+            <li>Platform:&nbsp;{
               game.platforms.map((p, i) => {
-                return <li key={i}>{p}</li>
+                return <li key={i}>{p}&nbsp;</li>
               })
               }</li>
           </ul>
         </div>
-        <div>
+        <div className="flex justify-self-end self-center">
         <a
         className="text-slate-300 text-2xl mt-12 max-w-4xl hover:scale-125 transition duration-500 ease-in-out hover:text-sky-800"
         href={game.website}
